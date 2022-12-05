9fef346739104a49d38da860a88d33fc5b85a424
dmgrd <117730998+dmgrd@users.noreply.github.com>
2022-12-05 14:17:14 +0300

    Add files via upload

---
+++ block 1.3.pas
@@ -0,0 +1,21 @@
+﻿Program block13;
+var
+  mas:array [1..20] of integer;
+  i,f,x,nmax,max: integer;
+  p:=1;
+
+  begin
+max:= mas[1]; nmax:=1;
+    f:=0;
+    for i:=1 to 20 do
+      begin
+    mas[i]:=random(-52,65);
+    writeln (i,': ',mas[i]);    
+if mas[i]>Max then
+begin
+Max:=mas[i];
+Nmax:=i;
+end;
+end;
+write(' Max=',max,' Nmax=',Nmax);
+end.
